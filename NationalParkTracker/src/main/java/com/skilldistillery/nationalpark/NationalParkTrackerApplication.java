3e1dd4cf1943c1c95314c04e574fd4d22790cafb
Emily Lau <emilylau@Brucebs-MacBook-Pro-2.local>
2019-08-02 16:05:48 -0600

    stub and test boot REST project

---
+++ NationalParkTracker/src/main/java/com/skilldistillery/nationalpark/NationalParkTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.nationalpark;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NationalParkTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NationalParkTrackerApplication.class, args);
+	}
+
+}
